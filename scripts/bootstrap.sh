82a96e50ba9da54fc35a49d6f3bbd2a11a209337
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-03-13 15:09:46 +0000

    Use alphagov fork of CKAN

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ! -z $1 && $1 == '2.8' ]]; then
-    CKAN_VERSION=2.8.3
+    CKAN_VERSION=2.8.3-dgu
     SRC_DIR=2.8
 else
     CKAN_VERSION=2.7.6
@@ -11,10 +11,13 @@ fi
 mkdir -p src/$SRC_DIR
 cd src/$SRC_DIR
 
-git clone --branch ckan-$CKAN_VERSION https://github.com/ckan/ckan
+# This should match settings in Dockerfile.dev in ckan project
+HARVEST_SHA=713a0fb04ff790d847824df427fa40a3f91bbc7c
+
+git clone --branch ckan-$CKAN_VERSION https://github.com/alphagov/ckan
 
 git clone https://github.com/alphagov/ckanext-datagovuk
-git clone --branch dgu-fixes https://github.com/alphagov/ckanext-harvest
+git clone https://github.com/ckan/ckanext-harvest
+(cd ckanext-harvest && git checkout $HARVEST_SHA)
 git clone --branch dgu-fixes https://github.com/alphagov/ckanext-spatial
-git clone https://github.com/ckan/ckanext-dcat
 git clone https://github.com/geopython/pycsw.git --branch 2.4.0 
